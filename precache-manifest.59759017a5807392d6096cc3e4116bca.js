cbd093eb25845d58d7f095b669b9ed7f2c974998
Rana <ranaelhefnawy9@gmail.com>
2020-06-30 22:34:26 +0900

    Updates

---
+++ precache-manifest.59759017a5807392d6096cc3e4116bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed984c85751be5004344789433a65124",
+    "url": "/Movies-IMDB/index.html"
+  },
+  {
+    "revision": "df5fd1208bd5f2cd30bc",
+    "url": "/Movies-IMDB/static/css/main.261a9f92.chunk.css"
+  },
+  {
+    "revision": "a8aaa306b396a04950b9",
+    "url": "/Movies-IMDB/static/js/2.01a1031b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movies-IMDB/static/js/2.01a1031b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5fd1208bd5f2cd30bc",
+    "url": "/Movies-IMDB/static/js/main.2a66d66c.chunk.js"
+  },
+  {
+    "revision": "7433f8a9336b01dddd40",
+    "url": "/Movies-IMDB/static/js/runtime-main.f714c3aa.js"
+  }
+]);
